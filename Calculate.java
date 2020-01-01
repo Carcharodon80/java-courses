4086c363f17319ff5df4c3b62f280e976367c466
Akulov Roman <karna241280@gmail.com>
2020-01-02 02:31:15 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,10 @@
-public class Calculate{
-    public static void main(String[] args){
+public class Calculate {
+    public static void main(String[] args) {
         System.out.println("calculate...");
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int sum = first + second;
+        System.out.println("Sum = " + sum);
     }
 
 }
